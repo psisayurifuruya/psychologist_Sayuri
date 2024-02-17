f22aaef7f109edf5a8888eb8a37bd8552192dd32
psisayurifuruya <psi.sayurifuruya@gmail.com>
2024-02-17 19:01:16 -0300

    commit-Reformulação do site

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function toggleMode() {
   // html.classList.add('light')
   //}
   //  OU
-  html.classList.toggle("light")
+  html.classList.toggle("dark")
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
   //substituir a imagem
-  if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
+  if (html.classList.contains("dark")) {
+    // se tiver dark mode, adicionar a imagem moon
     img.setAttribute("src", "./assets/say.png")
   } else {
-    // se tiver sem light mode, manter a imagem normal
+    // se tiver sem dark mode, manter a imagem normal
     img.setAttribute("src", "./assets/say.png")
   }
 }
